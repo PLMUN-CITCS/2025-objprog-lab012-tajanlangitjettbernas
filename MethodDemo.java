3c8b19ef238460a920c2bd39f19b7e6976330850
tajanlangitjettbernas <tajanlangitjett_bsit@plmun.edu.ph>
2025-02-19 14:31:48 +0800

    Submitting OBJPROG Week 05 - Laboratory # 12

---
+++ MethodDemo.java
@@ -0,0 +1,18 @@
+public class MethodDemo {
+    public static void main(String[] args) {
+greet(); // Calling the greet method
+}
+    }
+public static void greet() {
+    System.out.println("Hello from the greet method!"); 
+
+ }
+public class MethodDemo {
+    //... (main and greet methods)...
+
+    public static void printMessage(String message) {
+ System.out.println(message);
+printMessage("This is my message!");
+    }
+}
+}
